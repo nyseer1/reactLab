d95b29f260b18babf8af5e8cc33c6b2fb53b2e57
Nyseer Couse <nyseer.couse@gmail.com>
2023-09-15 16:11:14 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,80 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css';
+
+const studentList = [
+  {
+    firstName: "Misty",
+    lastName: "Knight",
+    sId: "234",
+    school: "Queens College",
+    major: "Law",
+  },
+  {
+    firstName: "Jessica",
+    lastName: "Jones",
+    sId: "434",
+    school: "Brooklyn College",
+    major: "CS",
+  },
+  {
+    firstName: "Colleen",
+    lastName: "Wing",
+    sId: "233",
+    school: "Queens College",
+    major: "CS",
+  },
+  {
+    firstName: "Dare",
+    lastName: "Devil",
+    sId: "876",
+    school: "CCNY",
+    major: "Law",
+  },
+  {
+    firstName: "Luke",
+    lastName: "Cage",
+    sId: "323",
+    school: "CCNY",
+    major: "Math",
+  },
+];
+
+function StudentInfo(props) {
+  return (
+    <div>
+      <div>
+        {props.lastName}, {props.firstName}
+      </div>
+      <ul>
+        <li>
+          <strong>ID:</strong> {props.sId}
+        </li>
+        <li>
+          <strong>School:</strong> {props.school}
+        </li>
+        <li>
+          <strong>Major:</strong> {props.major}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function ClassList() {
+  return (
+    <div>
+      <h1 className="red-text">Welcome to CTP</h1>
+      <p>List of Students</p>
+      {studentList.map((student) => (
+        <StudentInfo {...student} key={student.sId} />
+      ))}
+    </div>
+  );
+}
+
+
+
+const htmlContainer = document.getElementById("root");
+const root = ReactDOM.createRoot(htmlContainer);
+root.render(<ClassList />);
